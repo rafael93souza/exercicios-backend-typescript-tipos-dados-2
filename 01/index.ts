e0c276657df8e15bdf78bc09d050fe457d8920da
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-07-03 22:30:14 -0300

    :sparkles: feat: Exercise 01 finished

---
+++ 01/index.ts
@@ -0,0 +1,30 @@
+import fs from 'fs/promises';
+
+export async function readFile(){
+    try {
+        const file = await fs.readFile('bd.json', 'utf8');
+        const data = JSON.parse(file);
+        return data
+    } catch (error) {
+        if(error instanceof Error){
+             throw new Error(error.message);    
+        }
+        throw new Error('Something unexpected happened while reading the file');    
+    }
+}
+
+export async function writeFile(data:unknown){
+    try {
+        const dataWrite = JSON.stringify(data);
+        await fs.writeFile('bd.json', dataWrite);
+    } catch (error) {
+        if(error instanceof Error){
+             throw new Error(error.message);    
+        }
+        throw new Error('Something unexpected happened while writing the file');    
+    }
+}
+writeFile({
+    name: "Rafael Souza", 
+    teste:"New test"
+})
